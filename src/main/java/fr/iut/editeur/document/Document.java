e84bfebc9f98e18fdd518dcb3b4148b6909646b8
De-Murcia Enzo <enzo.de-murcia@etu.umontpellier.fr>
2023-11-08 14:33:23 +0000

    merge: development into master

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -1,5 +1,7 @@
 package fr.iut.editeur.document;
 
+import fr.iut.editeur.commande.CommandeDocument;
+
 public class Document {
 
     private String texte;
@@ -24,6 +26,19 @@ public class Document {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(end);
         texte = leftPart + remplacement + rightPart;
+    }
+
+    public void majuscules(int start, int end) {
+        String maj = (texte.substring(start, end)).toUpperCase();
+        remplacer(start, end, maj);
+    }
+
+    public void effacer(int start, int end) {
+        remplacer(start, end, "");
+    }
+
+    public void clear(){
+        setTexte("yaya");
     }
 
     @Override
